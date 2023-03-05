8f74c9766269ded95b09c9008995b09b7c80813e
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2023-03-05 16:10:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var io = require('socket.io')(server, {
   handlePreflightRequest: (req, res) => {
     const headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": ["http://localhost:" + port + "", "http://127.0.0.1:" + port + "", "https://cobalt.cosmicstar37.repl.co:443","http://147.185.221.180:35473","180.ip.ply.gg:35473","must-psychiatry.at.ply.gg:35473"],
+      "Access-Control-Allow-Origin": ["http://localhost:" + port + "", "http://127.0.0.1:" + port + "", "https://cobalt.cosmicstar37.repl.co:443","http://147.185.221.180:35473","http://180.ip.ply.gg:35473","http://must-psychiatry.at.ply.gg:35473"],
       "Access-Control-Allow-Credentials": true,
     };
     if (headers["Access-Control-Allow-Origin"] != headers["Access-Control-Allow-Origin"][0] || headers["Access-Control-Allow-Origin"][1] || headers["Access-Control-Allow-Origin"][2] || headers["Access-Control-Allow-Origin"][3] || headers["Access-Control-Allow-Origin"][4] || headers["Access-Control-Allow-Origin"][5]) {
@@ -72,37 +72,10 @@ var io = require('socket.io')(server, {
       res.writeHead(200, headers);
       res.end();
     }
-    /*if (req.session.uid === undefined && headers["Access-Control-Allow-Origin"] == headers["Access-Control-Allow-Origin"][0] || headers["Access-Control-Allow-Origin"][1] || headers["Access-Control-Allow-Origin"][2] || headers["Access-Control-Allow-Origin"][3] || headers["Access-Control-Allow-Origin"][4] || headers["Access-Control-Allow-Origin"][5]) {
-      res.status(200).render();
-      res.writeHead(200, headers);
-      res.redirect('/set_session');
-    }*/
   },
 });
 io.set("transports", ["websocket"]);
 
-// initializing express-session middleware
-// TODO: finish implementation later.
-/*var session_dir = './tmp/session';
-if (!fs.existsSync(session_dir)){
-    fs.mkdirSync(session_dir, { recursive: true });
-}
-var Session = require('express-session');
-var SessionStore = require('session-file-store')(Session);
-var session = Session({store: new SessionStore({path: __dirname+'/tmp/sessions'}), secret: '$!$@-bonzi_token_cookie$597-453#78-&%89@!_', resave: true, saveUninitialized: true});
-app.use(session); // session support
-app.get('/set_session', function (req, res) { // navigate here to set a new session
-  req.session.uid = Date.now();
-  //res.redirect(req.header('Referer') || '/');
-  res.redirect('back');
-});
-app.get('/get_session', function (req, res) { // navigate here to see session ID
-  res.end("Session id: "+req.session.uid);
-});
-io.use(ios(session)); // session support
-io.on('connection', function(socket){
-  socket.emit('message', "Session id: " + socket.handshake.session.uid); // this will echo session above
-});*/
 
 // Variable for toggling Replit mode
 const isReplit = settings.isReplit;
