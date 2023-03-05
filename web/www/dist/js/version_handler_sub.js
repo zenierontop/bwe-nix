f460e13dd7263a23c6fe79d9b821a05af8808c7e
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2023-03-05 12:40:52 -0500

    Update version_handler_sub.js

--- web/www/dist/js/version_handler_sub.js
+++ web/www/dist/js/version_handler_sub.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $.getJSON("../../dist/json/version.json", function(result){
       $.each(result, function(i, field){
-        $(".ver").html("BonziWORLD Enhanced  v" + field + " ");
+        $(".ver").text("BonziWORLD Enhanced  v" + field + " ");
     });
   });
 });
